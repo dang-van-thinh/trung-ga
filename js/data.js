dc6fe19487756d4b7a1812f878c17d84c1addc6b
Dang Van Thinh <110388496+dang-van-thinh@users.noreply.github.com>
2026-06-02 08:20:38 +0700

    update product

--- js/data.js
+++ js/data.js
@@ -240,37 +240,37 @@ const siteData = {
             image: "source/qua-1.png",
             featured: false
         },
-        {
-            id: "6-hop",
-            name: "COMBO 6 HỘP TẶNG 2 HỘP",
-            originalPrice: 956000,
-            priceValue: 717000,
-            priceDisplay: "717.000đ",
-            specs: [
-                "✓ 8 hộp x 12 quả",
-                "✓ Miễn phí ship toàn quốc",
-                "✓ Bảo hành 1 đổi 1 (lỗi do vận chuyển)"
-            ],
-            highlightSpec: true,
-            image: "source/qua-1.png",
-            featured: false
-        },
         // {
-        //     id: "trung-so",
-        //     name: "COMBO 5 HỘP TẶNG 2 (TRỨNG SO)",
-        //     originalPrice: 836500,
-        //     priceValue: 597500,
-        //     priceDisplay: "597.500đ",
+        //     id: "6-hop",
+        //     name: "COMBO 6 HỘP TẶNG 2 HỘP",
+        //     originalPrice: 956000,
+        //     priceValue: 717000,
+        //     priceDisplay: "717.000đ",
         //     specs: [
-        //         "✓ 7 hộp x 12 quả",
+        //         "✓ 8 hộp x 12 quả",
         //         "✓ Miễn phí ship toàn quốc",
-        //         "✓ Bảo hành 1 đổi 1 (lỗi do vận chuyển)",
-        //         "Chỉ áp dụng cho sản phẩm trứng so (trứng nhỏ)"
+        //         "✓ Bảo hành 1 đổi 1 (lỗi do vận chuyển)"
         //     ],
         //     highlightSpec: true,
         //     image: "source/qua-1.png",
         //     featured: false
         // },
+        {
+            id: "trung-so",
+            name: "COMBO 5 HỘP TẶNG 2 (TRỨNG SO)",
+            originalPrice: 836500,
+            priceValue: 597500,
+            priceDisplay: "597.500đ",
+            specs: [
+                "✓ 7 hộp x 12 quả",
+                "✓ Miễn phí ship toàn quốc",
+                "✓ Bảo hành 1 đổi 1 (lỗi do vận chuyển)",
+                "Chỉ áp dụng cho sản phẩm trứng so (trứng nhỏ)"
+            ],
+            highlightSpec: true,
+            image: "source/qua-1.png",
+            featured: false
+        },
 
     ],
 
